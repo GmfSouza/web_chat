a137538345274e8bc5282815e0bd92ba44a97843
Gabryel <gabryel.macedo@gmail.com>
2025-05-15 23:40:41 -0300

    declaring and initializing a constructor and calling the updatePassword method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, UseGuards, Request } from "@nestjs/common";
+import { Controller, Get, UseGuards, Request, Patch, Body } from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UpdatePasswordDto } from "./dtos/update-password.dto";
+import { UsersService } from "./users.service";
 
 @Controller('users')
 export class UsersController {
+constructor(private readonly usersService: UsersService) {}
+
     @UseGuards(JwtAuthGuard)
     @Get('profile')
-    getProfile(@Request() req) {
+    public async getProfile(@Request() req) {
         return {
-            message: 'Authenticated user',
+            message: 'Authenticated_user',
             user: req.user,
         };
     }
 
+    @UseGuards(JwtAuthGuard)
+    @Patch('update/password')
+    public async updatePassword(@Request() req, @Body() dto: UpdatePasswordDto) {
+        await this.usersService.updatePassword(req.user.id, dto)
+        return {
+            message: 'Password_updated_successfully'
+        }
+    }
 }
